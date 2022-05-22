c4331379c8e7266ae2d6093e545116f7c3bc7a22
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-05-22 20:37:49 +0200

    solve iteration x, y, z

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,43 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
+  constructor(currentTime, intervalId) {
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    value.padStart(2, '0');
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    if (this.intervalId) {
+      return (this.currentTime = 0);
+    }
   }
 
   split() {
-    // ... your code goes here
+    minuteTwoDigit = computeTwoDigitNumber(getMinutes());
+    secondTwoDigit = computeTwoDigitNumber(this.getSeconds());
   }
 }
 
